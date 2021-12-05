73a683a06adec287297ad5c4989b4bda6d06ee5a
healthyred <jhl3@williams.edu>
2021-12-04 22:17:03 -0500

    added tables and front end

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,42 @@
 // App.tsx
 import { ChakraProvider } from "@chakra-ui/react";
-import Home from "./index";
+import Layout from "../components/Layout";
+// Import DAppProvider
+import { DAppProvider } from "@usedapp/core";
+import { Tabs, TabPanel, TabList, TabPanels, Tab } from "@chakra-ui/tabs";
 
 export default function App() {
-  return (
-    <div>
-        <Home/>
-    </div>
 
+  return (
+    <DAppProvider config={{}}>
+      <div style={{color: "white"}}>
+        <ChakraProvider>
+        <Layout>
+          <div>
+          <Tabs>
+        <TabList>
+          <Tab>Active Projects</Tab>
+          <Tab>Funded Projects</Tab>
+          <Tab>Finished Projects</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>TODO add table</p>
+          </TabPanel>
+          <TabPanel>
+            <p> 2 TODO add table</p>
+          </TabPanel>
+          <TabPanel>
+            <p>3 TODO add table</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
+          </div>
+      </Layout>
+        </ChakraProvider>
+      </div>    
+    </DAppProvider>
   );
 }
