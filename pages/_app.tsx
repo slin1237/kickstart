ca7f5594c04a35ca87ca587331619aea85fe276e
healthyred <jhl3@williams.edu>
2021-12-04 21:31:37 -0500

    changing to react

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+// App.tsx
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../components/Layout";
+
+export default function App() {
+  return (
+    <div>
+      fuck
+    {/* <ChakraProvider>
+      <Layout>
+        <p style={{ color: "white" }}>Hello, world!</p>
+      </Layout>
+    </ChakraProvider>       */}
+    </div>
+
+  );
+}
