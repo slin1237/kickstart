5ca11ef4b3d0b22d57366ce52df5b2e8f9b66718
healthyred <jhl3@williams.edu>
2021-12-05 12:34:07 -0500

    added table funding button

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -1,5 +1,5 @@
 import { Button, Box, Text } from "@chakra-ui/react";
-import { useEthers, useEtherBalance } from "@usedapp/core";
+import { useEthers, useEtherBalance, useContractCall } from "@usedapp/core";
 import { formatEther } from "@ethersproject/units";
 import Identicon from "./Identicon";
 
