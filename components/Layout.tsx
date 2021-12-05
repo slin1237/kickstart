a831dac87e71bbf8a7dea62c732aa19bd907f46d
healthyred <jhl3@williams.edu>
2021-12-04 23:02:58 -0500

    added button for shit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,18 @@
 // Layout.tsx
 import { ReactNode, FC} from "react";
-import { Flex } from "@chakra-ui/react";
-
-type Props = {
-  children?: ReactNode;
-};
+import { Flex, useDisclosure } from "@chakra-ui/react";
+import ConnectButton from "./ConnectButton";
+import { Tabs, TabPanel, TabList, TabPanels, Tab } from "@chakra-ui/tabs";
+import AccountModal from "./AccountModal";
 
 interface LayoutProps {
     children?: ReactNode;
 }
 
 const Layout: FC<LayoutProps> = ({children}) => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <Flex
           flexDirection="column"
@@ -19,7 +21,31 @@ const Layout: FC<LayoutProps> = ({children}) => {
           h="100vh"
           bg="gray.800"
         >
-          {children}
+
+        <ConnectButton handleOpenModal={onOpen} />
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+            <div>
+          <Tabs>
+        <TabList>
+          <Tab>Active Projects</Tab>
+          <Tab>Funded Projects</Tab>
+          <Tab>Finished Projects</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>TODO add table</p>
+          </TabPanel>
+          <TabPanel>
+            <p> 2 TODO add table</p>
+          </TabPanel>
+          <TabPanel>
+            <p>3 TODO add table</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
+          </div>
         </Flex>
       ) 
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/Layout";
 // Import DAppProvider
 import { DAppProvider } from "@usedapp/core";
-import { Tabs, TabPanel, TabList, TabPanels, Tab } from "@chakra-ui/tabs";
 
 export default function App() {
 
@@ -11,30 +10,7 @@ export default function App() {
     <DAppProvider config={{}}>
       <div style={{color: "white"}}>
         <ChakraProvider>
-        <Layout>
-          <div>
-          <Tabs>
-        <TabList>
-          <Tab>Active Projects</Tab>
-          <Tab>Funded Projects</Tab>
-          <Tab>Finished Projects</Tab>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel>
-            <p>TODO add table</p>
-          </TabPanel>
-          <TabPanel>
-            <p> 2 TODO add table</p>
-          </TabPanel>
-          <TabPanel>
-            <p>3 TODO add table</p>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-
-          </div>
-      </Layout>
+          <Layout/>
         </ChakraProvider>
       </div>    
     </DAppProvider>
